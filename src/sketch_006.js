9a4f5dc2f129e9da94ca1a1300281843a57962b3
yanndepps <yann.deperrot@protonmail.com>
2021-09-14 14:37:10 +0200

    classes

--- src/sketch_006.js
+++ src/sketch_006.js
@@ -14,7 +14,7 @@ const sketch = () => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
     // ---
-    const point = { x: 800, y: 400, radius: 10 };
+    const point = new Point(800, 400, 10);
 
     context.beginPath();
     context.arc(point.x, point.y, point.radius, 0, Math.PI * 2);
@@ -24,3 +24,13 @@ const sketch = () => {
 };
 
 canvasSketch(sketch, settings);
+
+// --- Classes ---
+
+class Point {
+  constructor(x, y, radius) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+  }
+}
