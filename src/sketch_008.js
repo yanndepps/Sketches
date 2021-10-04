41900167fa63f793502aba05b95be1b7106a85fc
yanndepps <yann.deperrot@protonmail.com>
2021-10-04 22:03:09 +0200

    glyphs + end S008

--- src/sketch_008.js
+++ src/sketch_008.js
@@ -3,6 +3,7 @@
  */
 
 const canvasSketch = require('canvas-sketch');
+const rnd = require('canvas-sketch-util/random');
 
 const settings = {
   dimensions: [1080, 1080]
@@ -42,7 +43,7 @@ const sketch = ({ context, width, height }) => {
     typeContext.fillStyle = 'black';
     typeContext.fillRect(0, 0, cols, rows);
 
-    fontSize = cols;
+    fontSize = cols * 1.2;
 
     typeContext.fillStyle = 'white';
     typeContext.font = `${fontSize}px "bauhaus"`;
@@ -70,7 +71,13 @@ const sketch = ({ context, width, height }) => {
 
     const typeData = typeContext.getImageData(0, 0, cols, rows).data;
 
-    context.drawImage(typeCanvas, 0, 0);
+    context.fillStyle = 'black';
+    context.fillRect(0, 0, width, height);
+
+    context.textBaseLine = 'middle';
+    context.textAlign = 'center';
+
+    // context.drawImage(typeCanvas, 0, 0);
 
     for (let i = 0; i < numCells; i++) {
       const col = i % cols;
@@ -84,7 +91,12 @@ const sketch = ({ context, width, height }) => {
       const b = typeData[i * 4 + 2];
       const a = typeData[i * 4 + 3];
 
-      context.fillStyle = `rgb(${r}, ${g}, ${b})`;
+      const glyph = getGlyph(r);
+
+      context.font = `${cell * 2}px "bauhaus"`;
+      if (Math.random() < 0.1) context.font = `${cell * 6}px "bauhaus"`;
+
+      context.fillStyle = 'white';
 
       context.save();
       context.translate(x, y);
@@ -92,13 +104,22 @@ const sketch = ({ context, width, height }) => {
 
       // context.fillRect(0, 0, cell, cell);
 
-      context.beginPath();
-      context.arc(0, 0, cell * 0.5, 0, Math.PI * 2);
-      context.fill();
+      context.fillText(glyph, 0, 0);
 
       context.restore();
     }
   };
+};
+
+const getGlyph = (v) => {
+  if (v < 50) return '';
+  if (v < 100) return '.';
+  if (v < 150) return '-';
+  if (v < 200) return '+';
+
+  const glyphs = '_=/'.split('');
+
+  return rnd.pick(glyphs);
 };
 
 const onKeyUp = (e) => {
