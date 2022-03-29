3ae87d37c36df0d54a32876800f3ba3b3e4a02d5
yanndepps <yann.deperrot@protonmail.com>
2022-03-29 18:18:21 +0200

    The Nature Of Code -> init chapter 1

---
+++ src/sketch_017.js
@@ -0,0 +1,24 @@
+/*
+ * The Nature Of Code
+ * Chapter #1
+ * 1.4 -> Static Functions
+ */
+
+const canvasSketch = require('canvas-sketch');
+const p5 = require('p5');
+new p5();
+
+const settings = {
+  p5: true,
+  dimensions: [512, 512],
+  animate: true,
+  context: '2d'
+};
+
+const sketch = () => {
+  return ({ context, width, height }) => {
+    background(33);
+  };
+};
+
+canvasSketch(sketch, settings);
