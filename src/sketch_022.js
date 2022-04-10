5510404a46073eb877fd32b6a4611b332469994d
yanndepps <yann.deperrot@protonmail.com>
2022-04-10 22:33:15 +0200

    sketch_022 -> nice-color-palette

--- src/sketch_022.js
+++ src/sketch_022.js
@@ -5,42 +5,43 @@
  */
 
 const canvasSketch = require('canvas-sketch');
-// const palettes = require('nice-color-palettes/1000.json');
-// const rnd = require('canvas-sketch-util/random');
+const palettes = require('nice-color-palettes/1000.json');
+const rnd = require('canvas-sketch-util/random');
 // const { lerp } = require('canvas-sketch-util/math');
 const p5 = require('p5');
 new p5();
 
 const settings = {
   p5: true,
-  // dimensions: [550, 550],
   dimensions: [1024, 1024],
   context: '2d',
   animate: false
 };
 
-// let nColor = rnd.rangeFloor(2, 6);
-// let palette = rnd.shuffle(rnd.pick(palettes)).slice(0, nColor);
-// console.log(palette);
+let nColor = rnd.rangeFloor(2, 11);
+let palette = rnd.shuffle(rnd.pick(palettes)).slice(0, nColor);
 // let color = rnd.pick(palette);
-let palette = ["#f94144",
-  "#f3722c",
-  "#f8961e",
-  "#f9c74f",
-  "#f9844a",
-  "#90be6d",
-  "#43aa8b",
-  "#4d908e",
-  "#577790",
-  "#277da1"];
+// let palette = ["#f94144",
+//   "#f3722c",
+//   "#f8961e",
+//   "#f9c74f",
+//   "#f9844a",
+//   "#90be6d",
+//   "#43aa8b",
+//   "#4d908e",
+//   "#577790",
+//   "#277da1"];
 
-const sketch = (initialProps) => {
-  console.log(initialProps);
+const sketch = () => {
   colorMode(HSB, 360, 100, 100, 100);
+  noStroke();
+  // stroke(0, 0, 20, 100);
+  // strokeWeight(0.25);
+  smooth(2);
   // ---
   return ({ width, height }) => {
-    // background(palette[0]);
-    background(0, 0, 20, 100);
+    background(palette[0]);
+    // background(0, 0, 20, 100);
     angleMode(DEGREES);
     // ---
     let offset = width / 15;
@@ -55,12 +56,13 @@ const sketch = (initialProps) => {
 
 function separateGrid(x, y, w, minW) {
   let step = int(random(1, 5));
-  let d = round(w / step);
+  let d = ceil(w / step);
   // console.log('d -> ', d);
   for (let j = 0; j < step; j++) {
     for (let i = 0; i < step; i++) {
-      let x2 = x + i * d;
-      let y2 = y + j * d;
+      let x2 = ceil(x + i * d);
+      // console.log(x2);
+      let y2 = ceil(y + j * d);
       if (random() < 0.9 && d > minW) {
         separateGrid(x2, y2, d, minW);
       } else {
@@ -87,13 +89,13 @@ function drawShapes(cx, cy, d) {
       push();
       translate(-d / 2, 0);
       rotate(-90);
-      separateArc(0, 0, d, d, 0, 180, PIE, 2);
+      separateArc(0, 0, d, d, 0, 180, PIE, int(random(2, 5)));
       pop();
       // ---
       push();
       translate(d / 2, 0);
       rotate(90);
-      separateArc(0, 0, d, d, 0, 180, PIE, 2);
+      separateArc(0, 0, d, d, 0, 180, PIE, int(random(2, 5)));
       pop();
       break;
     case 2:
@@ -102,7 +104,7 @@ function drawShapes(cx, cy, d) {
       scale(random() > 0.5 ? -1 : 1, 1);
       translate(-d / 2 / 2, 0);
       rotate(-90);
-      separateArc(0, 0, d, d, 0, 180, PIE, 2);
+      separateArc(0, 0, d, d, 0, 180, PIE, int(random(2, 5)));
       pop();
       // ---
       push();
@@ -110,7 +112,7 @@ function drawShapes(cx, cy, d) {
       scale(random() > 0.5 ? -1 : 1, 1);
       translate(-d / 2 / 2, 0);
       rotate(-90);
-      separateArc(0, 0, d, d, 0, 180, PIE, 2);
+      separateArc(0, 0, d, d, 0, 180, PIE, int(random(2, 5)));
       pop();
       break;
     case 3:
@@ -160,7 +162,7 @@ function separateArc(x, y, w, h, startAngle, endAngle, type, step) {
 function setConicGradient(angle, x, y, palette) {
   let gradient = drawingContext.createConicGradient(angle, x, y);
   gradient.addColorStop(0, palette[0]);
-  gradient.addColorStop(1 / 2, palette[1]);
+  gradient.addColorStop(0.8, palette[1]);
   drawingContext.fillStyle = gradient;
 }
 
