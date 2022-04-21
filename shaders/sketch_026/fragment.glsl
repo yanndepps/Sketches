f1c3c535c9ef233143246a1d583f766145dfbb31
yanndepps <yann.deperrot@protonmail.com>
2022-04-21 23:35:49 +0200

    taoc -> ep_002

--- shaders/sketch_026/fragment.glsl
+++ shaders/sketch_026/fragment.glsl
@@ -5,17 +5,27 @@ precision highp float;
 uniform float time;
 uniform vec2 u_resolution;
 
-void main() {
-  // vec2 uv = gl_FragCoord.xy/u_resolution.xy;
-  // multiply one of the axes by the aspect ratio
-  // uv.x *= u_resolution.x/u_resolution.y;
+float Circle(vec2 uv, vec2 p, float r, float blur) {
+  float d = length(uv - p);
+  float c = smoothstep(r, r - blur, d);
+  return c;
+}
 
+void main() {
   vec2 uv = (gl_FragCoord.xy - u_resolution * 0.5) / u_resolution.yy + 0.5; // 0 <-> 1
   uv -= 0.5; // -0.5 <-> 0.5
 
-  float d = length(uv);
-  float r = 0.3;
-  float c = smoothstep(r, r-0.005, d);
+  vec3 col = vec3(.0);
+  float mask = Circle(uv, vec2(.0), .35, .05);
 
-  gl_FragColor=vec4(vec3(c), 1.0);
+  mask -= Circle(uv, vec2(-.1, .08), .06, .01);
+  mask -= Circle(uv, vec2(.1, .08), .06, .01);
+
+  float mouth = Circle(uv, vec2(.0), .3, .02);
+  mouth -= Circle(uv, vec2(.0, 0.1), .3, .02);
+  mask -= mouth;
+
+  // col = vec3(mouth);
+  col = vec3(1., 1., 0.) * mask;
+  gl_FragColor=vec4(col, 1.0);
 }
