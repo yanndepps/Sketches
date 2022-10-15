8623d69a00ec67158e2d3eed150664c4bc6dc23e
yanndepps <yann.deperrot@protonmail.com>
2022-10-15 21:50:25 +0200

    init cc_2.0 -> U4 PII

---
+++ src/sketch_052.js
@@ -0,0 +1,20 @@
+/*
+ * Creative Coding 2.0 -> animation, sound, color
+ * U4 -> Curves
+ * Part II
+ */
+
+const canvasSketch = require('canvas-sketch');
+
+const settings = {
+  dimensions: [1080, 1080]
+};
+
+const sketch = () => {
+  return ({ context, width, height }) => {
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+  };
+};
+
+canvasSketch(sketch, settings);
