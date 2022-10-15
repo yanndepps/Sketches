866e772bd6e23a79a492761a467eb81c9a24349f
yanndepps <yann.deperrot@protonmail.com>
2022-10-15 18:27:58 +0200

    cursor interaction

--- src/sketch_051.js
+++ src/sketch_051.js
@@ -6,15 +6,23 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [1080, 1080]
+  dimensions: [1080, 1080],
+  animate: true,
 };
 
-const sketch = () => {
-  const points = [
+let elCanvas;
+let points;
+
+const sketch = ({ canvas }) => {
+  points = [
     new Point({ x: 200, y: 540 }),
-    new Point({ x: 400, y: 300, control: true }),
+    new Point({ x: 400, y: 700, control: true }),
     new Point({ x: 880, y: 540 }),
   ];
+
+  canvas.addEventListener('mousedown', onMouseDown);
+
+  elCanvas = canvas;
 
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
@@ -29,6 +37,35 @@ const sketch = () => {
       point.draw(context);
     })
   };
+};
+
+const onMouseDown = (e) => {
+  window.addEventListener('mousemove', onMouseMove);
+  window.addEventListener('mouseup', onMouseUp);
+
+  const x = (e.offsetX / elCanvas.offsetWidth) * elCanvas.width;
+  const y = (e.offsetY / elCanvas.offsetHeight) * elCanvas.height;
+
+  points.forEach(point => {
+    point.isDragging = point.hitTest(x, y);
+  });
+};
+
+const onMouseMove = (e) => {
+  const x = (e.offsetX / elCanvas.offsetWidth) * elCanvas.width;
+  const y = (e.offsetY / elCanvas.offsetHeight) * elCanvas.height;
+
+  points.forEach(point => {
+    if (point.isDragging) {
+      point.x = x;
+      point.y = y;
+    }
+  });
+};
+
+const onMouseUp = () => {
+  window.removeEventListener('mousemove', onMouseMove);
+  window.removeEventListener('mouseup', onMouseUp);
 };
 
 canvasSketch(sketch, settings);
@@ -50,5 +87,13 @@ class Point {
     context.fill();
 
     context.restore();
+  }
+
+  hitTest(x, y) {
+    const dx = this.x - x;
+    const dy = this.y - y;
+    const dd = Math.sqrt(dx * dx + dy * dy);
+
+    return dd < 20;
   }
 }
