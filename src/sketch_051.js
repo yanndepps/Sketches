6c08be264ac7fc722f7feacc31547ce7129399d8
yanndepps <yann.deperrot@protonmail.com>
2022-10-15 20:32:30 +0200

    multiple points

--- src/sketch_051.js
+++ src/sketch_051.js
@@ -1,6 +1,7 @@
 /*
  * Creative Coding 2.0 -> animation, sound, color
  * U4 -> Curves
+ * Part I
  */
 
 const canvasSketch = require('canvas-sketch');
@@ -16,8 +17,10 @@ let points;
 const sketch = ({ canvas }) => {
   points = [
     new Point({ x: 200, y: 540 }),
-    new Point({ x: 400, y: 700, control: true }),
+    new Point({ x: 400, y: 700 }),
     new Point({ x: 880, y: 540 }),
+    new Point({ x: 600, y: 700 }),
+    new Point({ x: 640, y: 900 }),
   ];
 
   canvas.addEventListener('mousedown', onMouseDown);
@@ -28,11 +31,47 @@ const sketch = ({ canvas }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
     // ---
+    context.strokeStyle = '#999';
     context.beginPath();
     context.moveTo(points[0].x, points[0].y);
-    context.quadraticCurveTo(points[1].x, points[1].y, points[2].x, points[2].y);
+    // ---
+    for (let i = 1; i < points.length; i++) {
+      context.lineTo(points[i].x, points[i].y);
+    }
     context.stroke();
+    // ---
+    // context.beginPath();
+    // context.moveTo(points[0].x, points[0].y);
 
+    // for (let i = 1; i < points.length; i += 2) {
+    //   context.quadraticCurveTo(points[i + 0].x, points[i + 0].y, points[i + 1].x, points[i + 1].y);
+    // }
+    // context.stroke();
+    // ---
+    // draw midpoints
+    context.beginPath();
+
+    for (let i = 0; i < points.length - 1; i++) {
+      const curr = points[i + 0];
+      const next = points[i + 1];
+
+      const mx = curr.x + (next.x - curr.x) * 0.5;
+      const my = curr.y + (next.y - curr.y) * 0.5;
+
+      // context.beginPath();
+      // context.arc(mx, my, 5, 0, Math.PI * 2);
+      // context.fillStyle = 'blue';
+      // context.fill();
+
+      if (i == 0) context.moveTo(curr.x, curr.y);
+      else if (i == points.length - 2) context.quadraticCurveTo(curr.x, curr.y, next.x, next.y);
+      else context.quadraticCurveTo(curr.x, curr.y, mx, my);
+    }
+
+    context.lineWidth = 4;
+    context.strokeStyle = 'blue';
+    context.stroke();
+    // ---
     points.forEach(point => {
       point.draw(context);
     })
@@ -46,9 +85,13 @@ const onMouseDown = (e) => {
   const x = (e.offsetX / elCanvas.offsetWidth) * elCanvas.width;
   const y = (e.offsetY / elCanvas.offsetHeight) * elCanvas.height;
 
+  let hit = false;
   points.forEach(point => {
     point.isDragging = point.hitTest(x, y);
+    if (!hit && point.isDragging) hit = true;
   });
+
+  if (!hit) points.push(new Point({ x, y }));
 };
 
 const onMouseMove = (e) => {
