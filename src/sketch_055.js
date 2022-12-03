c164edc14ceb1b679676dbb79c6aa1e27d04c1fa
yanndepps <yann.deperrot@protonmail.com>
2022-12-03 09:59:40 +0100

    clean up

--- src/sketch_055.js
+++ src/sketch_055.js
@@ -11,6 +11,10 @@ const settings = {
 };
 
 const sketch = () => {
+  const audio = document.createElement('audio');
+  audio.src = '';
+  audio.play();
+
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
